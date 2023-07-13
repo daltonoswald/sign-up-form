370d91a669a6053dcf7dc2baae30ab846ae5106b
daltonoswald <daltonoswald@gmail.com>
2023-07-13 14:32:23 -0400

    added mostly everything needed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sign-up-form
 The Odin Project Sign-up Form
+This is a project to work with forms and practice different form elements and CSS.
---
+++ javascript.js
@@ -0,0 +1,41 @@
+const myform = document.getElementById('myform');
+myform.noValidate = true;
+
+myform.addEventListener('submit', validateForm);
+
+function validateForm(e) {
+    const form = e.target;
+    if (form.checkValidity()) {
+
+    } else {
+        e.preventDefault();
+        Array.from(form.elements).forEach(i => {
+            if (i.checkValidity()) {
+                i.parentElement.classList.remove('invalid');
+            } else {
+                i.parentElement.classList.add('invalid');
+            }
+        });
+        if (this.classList.contains('invalid'));
+        alert("Please fill out required fields");
+    }
+};
+
+function checkPassword(form) {
+    password1 = form.password1.value;
+    password2 = form.password2.value;
+
+    // if (password1 == '')
+    // alert("Please enter your password");
+    // else if (password2 == '')
+    // alert("Please confirm your password")
+
+    if (password1 != password2) {
+        alert("Passwords do not match")
+        return false;
+    }
+    else {
+        // alert("Passwords match")
+        return true;
+    }
+}
